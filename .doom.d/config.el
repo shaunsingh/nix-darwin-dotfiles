36220ed7ca32c8c400f5a363ea025726220443a9
shaunsingh <71196912+shaunsingh@users.noreply.github.com>
2021-05-12 10:37:55 -0400

    Update config.el

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,64 +1,51 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-
 ;;personal info
 (setq user-full-name "Shaurya Singh"
       user-mail-address "shaunsingh@gmail.com")
 
 ;;fonts
-(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 12)
-      doom-variable-pitch-font (font-spec :family "Verdana" :size 12)
-      ivy-posframe-font (font-spec :family "FiraCode Nerd Font" :size 13))
+(setq doom-font (font-spec :family "SF Mono" :size 12)
+      doom-variable-pitch-font (font-spec :family "SF Pro Display" :size 12.5)
+      ivy-posframe-font (font-spec :family "SF Mono" :size 12.5))
 
 ;;set theme
-;;(setq doom-theme 'doom-vibrant)
-(setq doom-theme 'doom-moonlight)
-;;(setq doom-theme 'doom-nord)
+(load-theme 'doom-moonlight t)
+
+;;neotree theme
+(setq doom-themes-neotree-file-icons t)
 
 (require 'ivy-posframe)
 ;; display at `ivy-posframe-style'
 (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-top-center)))
 (ivy-posframe-mode 1)
 
-;;minimap on startup
-;;(add-hook 'window-setup-hook #'minimap-mode)
-
 ;;org direcotry
 (setq org-directory "~/org/")
+(setq evil-want-fine-undo t)                       ; By default while in insert all changes are one big blob. Be more granular
 
-;;line numbers, trash, undo limits
-(setq display-line-numbers-type t)
-(setq-default
- delete-by-moving-to-trash t                      ; Delete files to trash
- window-combination-resize t                      ; take new window space from all other windows (not just current)
- x-stretch-cursor t)                              ; Stretch cursor to the glyph width
-
-(setq undo-limit 80000000                         ; Raise undo-limit to 80Mb
-      evil-want-fine-undo t                       ; By default while in insert all changes are one big blob. Be more granular
-      auto-save-default t                         ; Nobody likes to loose work, I certainly don't
-      truncate-string-ellipsis "…")               ; Unicode ellispis are nicer than "...", and also save /precious/ space
-
+;;modeline (icons, config, battery)
 (display-time-mode 1)                             ; Enable time in the mode-line
 (display-battery-mode 1)
-
-;;set split behavior
-(setq evil-vsplit-window-right t
-      evil-split-window-below t)
+(setq doom-modeline-icon (display-graphic-p))
+(setq doom-modeline-major-mode-icon t)
+(setq doom-modeline-major-mode-color-icon t)
+(setq doom-modeline-enable-word-count t)
 
 ;;default mode org mode
 (setq-default major-mode 'org-mode)
 
 ;;cute start screen
 (setq fancy-splash-image "/Users/shauryasingh/.doom.d/cute-demon.png")
-(setq +doom-dashboard-banner-padding '(0 . 3))
+(setq +doom-dashboard-banner-padding '(0 . 4))
 
-;;tell company to always do autocomplete + tabnine
+;;tell company to always do autocomplete
 (after! company
-  (setq company-idle-delay 0.5
+  (setq company-idle-delay 0.2
         company-dabbrev-downcase 0
-        company-minimum-prefix-length 1)
-  (setq company-show-numbers t)
-  (add-hook 'evil-normal-state-entry-hook #'company-abort)) ;; make aborting less annoying.
+        company-minimum-prefix-length 1
+        company-show-numbers t))
+;;
 (set-company-backend!
   '(text-mode
     markdown-mode
@@ -68,22 +55,12 @@
     company-files
     company-yasnippet))
 
-(setq-default history-length 1000)
-(setq-default prescient-history-length 1000)
-
-;;whichkey faster
-(setq which-key-idle-delay 0.5)
-
-;;make zen mode smaller
-;;(setq +zen-text-scale 0.8)
-
-;;treemacs smaller
-(add-hook 'treemacs-mode-hook (lambda () (text-scale-decrease 1)))
-(after! treemacs
-  (setq treemacs-width 30))
-
 ;;ligatures in org mode
 (setq +ligatures-in-modes '(org-mode))
 (setq +ligatures-extras-in-modes '(org-mode))
+
+;;java home for java-lsp
+(setenv "JAVA_HOME"  "/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home")
+(setq lsp-java-java-path "/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home/bin/java")
 
 (global-subword-mode 1)
