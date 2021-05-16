758b831863f572f39d84cf45f2146512d327f987
shaunsingh <71196912+shaunsingh@users.noreply.github.com>
2021-05-15 23:57:47 -0400

    Update config.el

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -2,38 +2,29 @@
 
 ;;personal info
 (setq user-full-name "Shaurya Singh"
-      user-mail-address "shaunsingh@gmail.com")
+      user-mail-address "shaunsingh0207@gmail.com")
 
 ;;fonts
 (setq doom-font (font-spec :family "SF Mono" :size 12)
-      doom-variable-pitch-font (font-spec :family "SF Pro Display" :size 12.5)
-      ivy-posframe-font (font-spec :family "SF Mono" :size 12.5))
+      doom-variable-pitch-font (font-spec :family "SF Pro Display" :size 12.5))
 
 ;;set theme
-(load-theme 'doom-moonlight t)
+(setq doom-theme 'doom-nord)
+;;(setq doom-theme 'doom-moonlight)
 
-;;neotree theme
-(setq doom-themes-neotree-file-icons t)
-
+;;ivy vs-code style
 (require 'ivy-posframe)
-;; display at `ivy-posframe-style'
 (setq ivy-posframe-display-functions-alist '((t . ivy-posframe-display-at-frame-top-center)))
-(ivy-posframe-mode 1)
 
-;;org direcotry
+;;org directory
 (setq org-directory "~/org/")
 (setq evil-want-fine-undo t)                       ; By default while in insert all changes are one big blob. Be more granular
 
 ;;modeline (icons, config, battery)
 (display-time-mode 1)                             ; Enable time in the mode-line
 (display-battery-mode 1)
-(setq doom-modeline-icon (display-graphic-p))
 (setq doom-modeline-major-mode-icon t)
-(setq doom-modeline-major-mode-color-icon t)
 (setq doom-modeline-enable-word-count t)
-
-;;default mode org mode
-(setq-default major-mode 'org-mode)
 
 ;;cute start screen
 (setq fancy-splash-image "/Users/shauryasingh/.doom.d/cute-demon.png")
@@ -42,10 +33,7 @@
 ;;tell company to always do autocomplete
 (after! company
   (setq company-idle-delay 0.2
-        company-dabbrev-downcase 0
-        company-minimum-prefix-length 1
-        company-show-numbers t))
-;;
+        company-dabbrev-downcase 0))
 (set-company-backend!
   '(text-mode
     markdown-mode
@@ -55,12 +43,19 @@
     company-files
     company-yasnippet))
 
-;;ligatures in org mode
-(setq +ligatures-in-modes '(org-mode))
-(setq +ligatures-extras-in-modes '(org-mode))
-
 ;;java home for java-lsp
 (setenv "JAVA_HOME"  "/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home")
 (setq lsp-java-java-path "/Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home/bin/java")
 
-(global-subword-mode 1)
+;;keybindings
+(map! :leader
+      :desc "hop to word"
+      "w w" #'avy-goto-word-0)
+
+
+(map! :leader
+      :desc "hop to line"
+      "l" #'avy-goto-line)
+
+;;(add-to-list 'default-frame-alist '(fullscreen . fullboth))
+;;(setq ns-use-native-fullscreen t)
