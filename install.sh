2e7a06abd88fc265c59359435e2b9a991e36acfc
shaunsingh <71196912+shaunsingh@users.noreply.github.com>
2021-06-11 13:01:43 -0400

    Removed backup, fixed install script

--- install.sh
+++ install.sh
@@ -4,13 +4,15 @@
 echo "Installing Homebrew and it's custom packages"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+echo "Enrolling in homebrew dev branch"
+export HOMEBREW_DEVELOPER=1
+
 #Installing Packages
 brew tap homebrew/cask-fonts
 brew tap homebrew/cask
 brew tap homebrew/bundle
-brew tap homeberw/core
+brew tap homebrew/core
 brew tap homebrew/services
-brew tap k-vernooy/tap
 brew tap railwaycat/emacsmacport
 
 brew install --cask font-fira-code-nerd-font
@@ -33,7 +35,6 @@ brew services start yabai
 brew services start skhd
 brew update
 brew upgrade
-brew services restart --all
 brew cleanup
 
 echo "Installing Latex Packages"
@@ -46,29 +47,6 @@ git clone https://github.com/shaunsingh/vimrc-dotfiles.git
 #Clone bar into default Übersicht location
 git clone https://github.com/zzzeyez/pecan.git "$HOME/Library/Application Support/Übersicht/widgets/pecan"
 cd vimrc-dotfiles
-
-echo "Creating a backup of all configs in ~/config_backup"
-
-mkdir $HOME/config_backup
-
-# following code is stolen from here: https://blog.sleeplessbeastie.eu/2013/03/12/simple-shell-script-to-backup-selected-directories/
-# Parent backup directory
-backup_parent_dir="$HOME/config_backup"
-
-# Directories to backup
-backup_me="$HOME/scripts/ $HOME/ .config/ $HOME/bin/ $HOME/spicetify_data/ $HOME/.zshrc $HOME/.skhdrc $HOME/.Xresources $HOME/Pictures $HOME/.yabairc"
-
-# Check and create backup directory
-backup_date=`date +%Y_%m_%d_%H_%M`
-backup_dir=${backup_parent_dir}/configs_${backup_date}
-mkdir -p $backup_dir
-
-# Perform backup
-for directory in $backup_me
-do
-        archive_name=`echo ${directory} | sed s/^\\\/// | sed s/\\\//_/g`
-        tar pcfzP ${backup_dir}/${archive_name}.tgz ${directory} 2>&1 | tee > ${backup_dir}/${archive_name}.log
-done
 
 echo "Installing Dotfiles from Cloned Repository"
 cp .config $HOME/.config
