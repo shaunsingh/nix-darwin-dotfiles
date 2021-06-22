99a016ccf99b77866e42ad802e6a4c19ab3cbbf3
shaunsingh <shaunsingh0207@gmail.com>
2021-06-22 09:24:02 -0400

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-echo " "
-echo "Warning: the following script will install multiple applications, packages, and files on your computer. Make sure to read through the file before proceding"
-echo " "
-echo " "
-read -n 1 -s -r -p "Press any key to continue"
-
 #Clone Dotfile Repo
 echo "Cloning Dotfiles"
 git clone --depth 1 https://github.com/shaunsingh/vimrc-dotfiles.git
