6914fa901b9c86bcab4d0a112e20126099c23810
shaunsingh <shaunsingh0207@gmail.com>
2021-06-22 00:03:42 -0400

    Fixed nix configuration

--- .nixpkgs/darwin-configuration.nix
+++ .nixpkgs/darwin-configuration.nix
@@ -8,29 +8,17 @@
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  
-  environment.systemPackages = with pkgs; [ vim cowsay lolcat curl git htop ripgrep neofetch alacritty starship fortune fish ranger aspell neovim-nightly emacsMacport ];
-
-  services = {
-    yabai.enable = true;
-    skhd.enable = true;
-  };
 
   nixpkgs.config.allowUnfree = true;
 
-  nixpkgs.overlays = [
-    (import (builtins.fetchTarball {
-      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
-    }))
-  ];
+  environment.systemPackages = with pkgs; [ vim ripgrep alacritty starship fortune cowsay fish ranger aspell ]; 
 
-  # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
   # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
 
   # Auto upgrade nix package and the daemon service.
-  # services.nix-daemon.enable = true;
-  nix.package = pkgs.nixUnstable;
+  services.nix-daemon.enable = true;
+  nix.package = pkgs.nix;
 
   # Create /etc/bashrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
