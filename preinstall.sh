e562c8dcbdba2ce044dd4139a97cca00db3fc67c
shaunsingh <shaunsingh0207@gmail.com>
2021-07-01 14:30:15 -0400

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 #Clone Dotfile Repo
-echo "Cloning Dotfiles"
-git clone --depth 1 https://github.com/shaunsingh/vimrc-dotfiles.git
+#echo "Cloning Dotfiles"
+#git clone --depth 1 https://github.com/shaunsingh/vimrc-dotfiles.git
+cd
 cd vimrc-dotfiles
 
 echo "Installing Dotfiles from Cloned Repository"
 cp -u -R .config ~
 cp -u -R .doom.d ~
-cp -u .zshrc .skhdrc .yabairc .ideavimrc .vimrc .gitconfig ~
+cp -u .vimrc .gitconfig ~
