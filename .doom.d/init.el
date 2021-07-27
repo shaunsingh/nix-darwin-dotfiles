cded307ba9f078ceff71327282c9bfbf2e5e701c
shaunsingh <shaunsingh0207@gmail.com>
2021-07-27 16:37:45 -0400

    pdf-tools improvements

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -81,10 +81,10 @@
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
-       ;;eshell            ; the elisp shell that works everywhere
+       eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       vterm             ; the best terminal emulation in Emacs
+       ;;vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
