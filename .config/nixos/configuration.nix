b4187bcb84a86bd4c696e5f75bbd3bf9816ce320
shaunsingh <shaunsingh0207@gmail.com>
2021-08-11 19:49:40 +0000

    revert

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -78,6 +78,7 @@
     # emacs config deps (latex, aspell)
     (aspellWithDicts (dicts: with dicts; [ en en-computers en-science ]))
     (pkgs.texlive.combine { inherit (pkgs.texlive) scheme-small dvipng dvisvgm l3packages xcolor soul adjustbox collectbox amsmath siunitx cancel mathalpha capt-of chemfig wrapfig mhchem latexmk; })
+
   ];
 
   # overlay for emacsGcc and Neovim-Nightly
@@ -93,7 +94,7 @@
   # use sway :chad:
   programs.sway = {
    enable = true;
-   extrapackages = with pkgs; [
+   extraPackages = with pkgs; [
      waybar
      dmenu
      swaylock
@@ -104,13 +105,22 @@
      wl-clipboard
      wf-recorder
    ];
-   extrasessioncommands = ''
-     export sdl_videodriver=wayland
-     export qt_qpa_platform=wayland
-     export qt_wayland_disable_windowdecoration="1"
-     export _java_awt_wm_nonreparenting=1
-     export moz_enable_wayland=1
+   extraSessionCommands = ''
+     export SDL_VIDEODRIVER=wayland
+     export QT_QPA_PLATFORM=wayland
+     export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+     export _JAVA_AWT_WM_NONREPARENTING=1
+     export MOZ_ENABLE_WAYLAND=1
    '';
+  };
+
+  # setup systemd etc
+  systemd.user.targets.sway-session = {
+    description = "Sway compositor session";
+    documentation = ["man:systemd.special(7)" ];
+    bindsTo = [ "graphical-session.target" ];
+    wants = ["graphical-session-pre.target" ];
+    after = [ "graphical-session-pre.target" ];
   };
 
   # battery life stuff
