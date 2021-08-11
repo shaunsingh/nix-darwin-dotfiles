0ed77d401cbb2eaf248c7a820c220c8f1c9d5784
shaunsingh <shaunsingh0207@gmail.com>
2021-08-11 19:43:24 +0000

    Remove some more wayland stuff

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -6,18 +6,22 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ # Include the results of the hardware scan, use cachix
       ./hardware-configuration.nix
       ./cachix.nix
     ];
 
+  # allow me to install unfree packages 
   nixpkgs.config.allowUnfree = true;
+
+  # use network manager
   networking.networkmanager.enable = true;
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # set the hostname
   networking.hostName = "nixos"; # Define your hostname.
 
   # Set your time zone.
@@ -45,8 +49,11 @@
 
   # List packages installed in system profile. To search, run:
   environment.systemPackages = with pkgs; [
+
+    # dotfiles management
     yadm 
 
+    # utils
     wget
     git
     gcc
@@ -55,19 +62,25 @@
     bat
     fd
 
+    # terminal stuff
     starship
     alacritty
     tmux
     neofetch 
 
+    # browser
     firefox
+
+    # editors
     neovim-nightly
     emacsPgtkGcc
 
+    # emacs config deps (latex, aspell)
     (aspellWithDicts (dicts: with dicts; [ en en-computers en-science ]))
     (pkgs.texlive.combine { inherit (pkgs.texlive) scheme-small dvipng dvisvgm l3packages xcolor soul adjustbox collectbox amsmath siunitx cancel mathalpha capt-of chemfig wrapfig mhchem latexmk; })
   ];
 
+  # overlay for emacsGcc and Neovim-Nightly
   nixpkgs.overlays = [
     (import (builtins.fetchTarball {
       url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
@@ -77,9 +90,10 @@
     }))
   ];
 
+  # use sway :chad:
   programs.sway = {
    enable = true;
-   extraPackages = with pkgs; [
+   extrapackages = with pkgs; [
      waybar
      dmenu
      swaylock
@@ -90,24 +104,19 @@
      wl-clipboard
      wf-recorder
    ];
-   extraSessionCommands = ''
-     export SDL_VIDEODRIVER=wayland
-     export QT_QPA_PLATFORM=wayland
-     export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-     export _JAVA_AWT_WM_NONREPARENTING=1
-     export MOZ_ENABLE_WAYLAND=1
+   extrasessioncommands = ''
+     export sdl_videodriver=wayland
+     export qt_qpa_platform=wayland
+     export qt_wayland_disable_windowdecoration="1"
+     export _java_awt_wm_nonreparenting=1
+     export moz_enable_wayland=1
    '';
   };
 
-  systemd.user.targets.sway-session = {
-    description = "Sway compositor session";
-    documentation = ["man:systemd.special(7)" ];
-    bindsTo = [ "graphical-session.target" ];
-    wants = ["graphical-session-pre.target" ];
-    after = [ "graphical-session-pre.target" ];
-  };
- 
+  # battery life stuff
   services.tlp.enable = true;
+
+  # Use fish, launch sway after login
   programs.fish = {
     enable = true;
     loginShellInit = ''
@@ -116,6 +125,8 @@
       end
     '';
   };
+
+  # Set default shell to sway
   users.defaultUserShell = pkgs.fish;
 
   system.stateVersion = "21.05"; # Did you read the comment?
