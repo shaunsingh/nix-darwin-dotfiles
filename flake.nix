9b64bd2f3d897335c0f8e14f856d87b8f9d74c74
shaunsingh <shaunsingh0207@gmail.com>
2021-08-18 22:04:56 +0000

    back to emacspgtkgcc

--- flake.nix
+++ flake.nix
@@ -3,8 +3,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     emacsNg-src.url = "github:emacs-ng/emacs-ng";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
   };
-  outputs = { nixpkgs, nixpkgs-unstable, emacsNg-src, ... }@inputs: {
+  outputs = { nixpkgs, nixpkgs-unstable, emacsNg-src, emacs-overlay, ... }@inputs: {
     nixosConfigurations = {
       shaunsingh-laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -12,6 +13,12 @@
           { _module.args = inputs; }
           ./nixos/configuration.nix
           ./nixos/hardware-configuration.nix
+          ({ pkgs, ... }: {
+            nixpkgs.overlays = [
+              emacs-overlay.overlay
+              (self: super: {
+                unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
+          })
         ];
       };
     };
