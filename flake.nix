654d762663507d69329b1938c283bd6e9a14fce1
shaunsingh <shaunsingh0207@gmail.com>
2021-08-18 18:50:20 +0000

    more flake stuff

--- flake.nix
+++ flake.nix
@@ -2,11 +2,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    emacs-overlay.url = "github:nix-community/emacs-overlay";
     emacsNg-src.url = "github:emacs-ng/emacs-ng"
   };
 
-  outputs = { nixpkgs, nixpkgs-unstable, emacs-overlay, ... }: {
+  outputs = { nixpkgs, nixpkgs-unstable, emacsNg-src, ... }: {
     nixosConfigurations = {
       shaunsingh-laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
