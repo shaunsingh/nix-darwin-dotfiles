50c9f0ceb85d8bc0f8b3b3e73ae2e2b72c00acc0
shaunsingh <shaunsingh0207@gmail.com>
2021-08-18 20:57:51 +0000

    more flake stuff

--- flake.nix
+++ flake.nix
@@ -4,14 +4,14 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     emacsNg-src.url = "github:emacs-ng/emacs-ng";
   };
-  outputs = { nixpkgs, nixpkgs-unstable, emacsNg-src, ... }: {
+  outputs = { nixpkgs, nixpkgs-unstable, emacsNg-src, ... }@inputs: {
     nixosConfigurations = {
       shaunsingh-laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           { _module.args = inputs; }
-          ./config/nixos/configuration.nix
-          ./config/nixos/hardware-configuration.nix
+          /home/shauryasingh/.config/nixos/configuration.nix
+          /home/shauryasingh/.config/nixos/hardware-configuration.nix
         ];
       };
     };
