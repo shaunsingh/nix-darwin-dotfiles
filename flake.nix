4e51bb700cdb92894e94786dbb6483f3e56e145d
shaunsingh <shaunsingh0207@gmail.com>
2021-08-19 02:13:26 +0000

    flake fixes

--- flake.nix
+++ flake.nix
@@ -1,12 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     emacsNg-src.url = "github:emacs-ng/emacs-ng";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    # neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
-  outputs = { nixpkgs, nixpkgs-unstable, emacsNg-src, emacs-overlay, neovim-nightly-overlay, ... }@inputs: {
+  outputs = { nixpkgs, emacsNg-src, emacs-overlay, neovim-nightly-overlay, ... }@inputs: {
     nixosConfigurations = {
       shaunsingh-laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -17,7 +16,7 @@
           ({ pkgs, ... }: {
             nixpkgs.overlays = [
               emacs-overlay.overlay
-              neovim-nightly-overlay.overlay
+              # neovim-nightly-overlay.overlay
             ];
           })
         ];
