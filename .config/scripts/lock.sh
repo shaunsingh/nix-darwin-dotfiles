de2a27b653138a41cf953fc6d7980e012d588c87
shaunsingh <shaunsingh0207@gmail.com>
2021-08-22 16:47:28 -0400

    fix lock screen

--- .config/scripts/lock.sh
+++ .config/scripts/lock.sh
@@ -1,11 +1,11 @@
 swaylock --screenshots --clock --indicator-idle-visible \
 	--indicator-radius 100 \
 	--indicator-thickness 7 \
-	--ring-color 242730 \
+	--ring-color 2a2e38 \
 	--key-hl-color 51afef \
-	--text-color FCCE7B \
-	--line-color 00000000 \
-	--inside-color 00000088 \
+	--text-color bbc2cf \
+	--line-color 2a2e38 \
+	--inside-color 242730 \
 	--separator-color 00000000 \
 	--fade-in 0.1 \
 	--effect-scale 0.5 --effect-blur 7x3 --effect-scale 2 \
