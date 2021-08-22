9701fc349a71d7b6db2738e8b13c80cc9cca5d55
shaunsingh <shaunsingh0207@gmail.com>
2021-08-22 16:41:09 -0400

    lock and idle

--- .config/scripts/idle.sh
+++ .config/scripts/idle.sh
@@ -1,5 +1,5 @@
 swayidle \
-	timeout 60 '~/.config/sway/lock.sh --grace 10 --fade-in 4' \
+	timeout 60 '~/.config/scripts/lock.sh --grace 10 --fade-in 4' \
 	timeout 80 'swaymsg "output * dpms off"' \
 	resume 'swaymsg "output * dpms on"' \
-	before-sleep '~/.config/sway/lock.sh'
+	before-sleep '~/.config/scripts/lock.sh'
--- .config/scripts/lock.sh
+++ .config/scripts/lock.sh
@@ -10,5 +10,3 @@ swaylock --screenshots --clock --indicator-idle-visible \
 	--fade-in 0.1 \
 	--effect-scale 0.5 --effect-blur 7x3 --effect-scale 2 \
 	--effect-vignette 0.5:0.5 \
-	--effect-compose "0,1.5%;-1x10%;$HOME/.config/sway/lock.svg" \
-	"$@"
