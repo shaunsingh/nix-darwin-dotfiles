67be68e5f0a1c84b96c9c6da342f4f3b8e145e26
shaunsingh <shaunsingh0207@gmail.com>
2021-09-15 19:04:58 -0400

    add rainbow delimeters

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -731,6 +731,22 @@ return require('packer').startup(function()
    }
 
    use {
+       "p00f/nvim-ts-rainbow",
+        after = "nvim-treesitter",
+        config = function()
+            require'nvim-treesitter.configs'.setup {
+              rainbow = {
+                enable = true,
+                extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+                max_file_lines = nil, -- Do not enable for files with more than n lines, int
+                -- colors = {}, -- table of hex strings
+                -- termcolors = {} -- table of colour name strings
+              }
+            }
+        end,
+   }
+
+   use {
       "lewis6991/gitsigns.nvim",
       config = function()
          local present, gitsigns = pcall(require, "gitsigns")
