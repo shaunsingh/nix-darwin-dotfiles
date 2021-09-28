322e773d008530d06b026cb9058e5f2415f21953
shaunsingh <shaunsingh0207@gmail.com>
2021-09-28 19:18:05 -0400

    (doom) move to math-preview

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -15,10 +15,11 @@
            :files ("*.el" "filters" "preprocessors")))
 
 ;;latex
-(package! org-fragtog)
+;;(package! org-fragtog)
 (package! aas :recipe (:host github :repo "ymarco/auto-activating-snippets"))
 (package! laas :recipe (:host github :repo "tecosaur/LaTeX-auto-activating-snippets"))
 (package! engrave-faces :recipe (:host github :repo "tecosaur/engrave-faces"))
+(package! math-preview)
 
 ;;markdown and html
 (package! ox-gfm)
