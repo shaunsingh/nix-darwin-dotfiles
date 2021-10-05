b83827603652bee4b242d0c7c055a20159457423
shaunsingh <shaunsingh0207@gmail.com>
2021-10-05 19:05:47 -0400

    (nix) try to move to flake

--- flake.nix
+++ flake.nix
@@ -1,23 +1,17 @@
 {
 
-  nixConfig = {
-
-    # use binaries for emacsng
-    extra-substituters = "https://emacsng.cachix.org";
-    extra-trusted-public-keys = "emacsng.cachix.org-1:i7wOr4YpdRpWWtShI8bT6V7lOTnPeI7Ho6HaZegFWMI=";
-
-  };
+  description = "Shaurya's Darwin System";
 
   inputs = {
 
     # use nixpkgs unstable
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-
-    # I prefer emacs-ng, but emacsPgtkGcc is also an option
-    emacsNg-src.url = "github:emacs-ng/emacs-ng";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable-darwin";
 
     # Grab the latest neovim and compile it
     neovim.url = "github:neovim/neovim?dir=contrib";
+
+    # Same with emacs
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
 
     # attempt to use sway-borders
     nixpkgs-overlays = {
@@ -25,38 +19,29 @@
         inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # Nix Darwin
+    darwin.url = "github:lnl7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+
   };
 
-  outputs = { self, nixpkgs, nixpkgs-overlays, emacsNg-src, neovim, ... }@inputs: {
-
-    overlay = final: prev: {
-      # use busybox coreutils (disabled for now)
-      oldutils = prev.coreutils; coreutils = final.busybox;
-    };
-
-    nixosConfigurations = {
-      # macbook 6,1 config
-      shaunsingh-laptop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          { _module.args = inputs; }
-
-          # use those extra packages as an overlay
-          { nixpkgs.overlays = [ nixpkgs-overlays.overlay ]; }
-
-          # source our two config files, obviously
-          ./nixos/configuration.nix
-          ./nixos/hardware-configuration.nix
-
-          # add the neovim overlay
-          ({ pkgs, ... }: {
-            nixpkgs.overlays = [
-              neovim.overlay
-              # self.overlay
-            ];
-          })
-        ];
-      };
+  outputs = { self, nixpkgs, darwin, nixpkgs-overlays, emacs-overlay, neovim, ... }@inputs: {
+    darwinConfigurations."shaunsingh-laptop" = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+        { _module.args = inputs; }
+        # use those extra packages as an overlay
+        { nixpkgs.overlays = [ nixpkgs-overlays.overlay ]; }
+        # source our two config files, obviously
+        ./nixos/darwin-configuration.nix
+        # add the neovim and emacs overlays 
+        ({ pkgs, ... }: {
+          nixpkgs.overlays = [
+            neovim.overlay
+            emacs-overlay.overlay
+          ];
+        })
+      ];
     };
   };
 }
