5367ac2e85a23f1315ee0c5686f0b1cdf877e3a1
shaunsingh <shaunsingh0207@gmail.com>
2021-10-06 00:15:47 -0400

    (yadm/bootstrap) tell doom to agree to all y by default

--- .config/yadm/bootstrap.d/emacs.sh
+++ .config/yadm/bootstrap.d/emacs.sh
@@ -4,6 +4,6 @@ set -eu
 
 emacs --batch --eval "(progn (require 'org) (setq org-confirm-babel-evaluate nil) (org-babel-tangle-file \"~/.config/doom/config.org\"))"
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-~/.emacs.d/bin/doom install
+~/.emacs.d/bin/doom -y install
 
 echo "Doom Emacs ✅"
