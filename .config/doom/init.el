7361b43241cfcae64e3f2fbb5e17aec5045c1fb1
shaunsingh <shaunsingh0207@gmail.com>
2021-10-07 15:16:37 -0400

    (doom) re-add formatting module

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -48,7 +48,7 @@
        (evil +everywhere)           ; come to the dark side, we have cookies
        file-templates               ; auto-snippets for empty files
        fold                         ; (nigh) universal code folding
-       format                       ; automated prettiness
+       (format +onsave)             ; automated prettiness
        ;;god                        ; run Emacs commands without modifier keys
        ;;lispy                      ; vim for lisp, for people who don't like vim
        ;;multiple-cursors           ; editing in many places at once
@@ -66,10 +66,10 @@
        vc                           ; version-control and Emacs, sitting in a tree
 
        :term
-       ;;eshell                     ; the elisp shell that works everywhere
+       eshell                       ; the elisp shell that works everywhere
        ;;shell                      ; simple shell REPL for Emacs
        ;;term                       ; basic terminal emulator for Emacs
-       vterm                        ; the best terminal emulation in Emacs
+       ;;vterm                      ; the best terminal emulation in Emacs
 
        :checkers
        syntax                       ; tasing you for every semicolon you forget
