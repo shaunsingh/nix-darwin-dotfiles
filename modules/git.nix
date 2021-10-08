75be7c4b630c7b1822dcb7c983a426f8e1c79295
shaunsingh <shaunsingh0207@gmail.com>
2021-10-07 23:11:10 -0400

    (nix) add htop config

--- modules/git.nix
+++ modules/git.nix
@@ -10,6 +10,34 @@
       ".DS_Store"
     ];
   };
+  home-manager.users.shauryasingh.programs.htop = {
+    color_scheme = 0;
+    cpu_count_from_one = 0;
+    delay = 15;
+    show_program+path = 1;
+    tree_view = 1;
+    show_cpu_usage = 1;
+    hide_function_bar = 2;
+    fields = with config.lib.htop.fields; [
+      PID
+      USER
+      PRIORITY
+      NICE
+      M_SIZE
+      M_RESIDENT
+      M_SHARE
+      STATE
+      PERCENT_CPU
+      PERCENT_MEM
+      TIME
+      COMM
+    ];
+    highlight_base_name = 0;
+    highlight_megabytes = 1;
+    highlight_threads = 1;
+    highlight_changes = 0;
+    highlight_changes_delay_secs = 5;
+  };
   home-manager.users.shauryasingh.programs.alacritty = {
     enable = true;
     settings = {
