1bdba378d6e0dbf16d706423d5b0d7918cbff168
shaunsingh <shaunsingh0207@gmail.com>
2021-10-08 20:24:27 -0400

    (nix/mu4e) remove redundant files

--- .config/doom/misc/org-css/.dir-locals.el
+++
@@ -1,6 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((scss-mode . ((after-save-hook . (lambda ()
-                                    (shell-command "./build.sh")
-                                    (org-html-reload-fancy-style))))))
--- .config/doom/modules/ui/tree-sitter/packages.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; ui/tree-sitter/packages.el
-
-(package! tree-sitter
-  :ignore (null (bound-and-true-p module-file-suffix))
-  :pin "7f5d0938002092ec08830a73f64961021303e1e9")
-(package! tree-sitter-langs
-  :ignore (null (bound-and-true-p module-file-suffix))
-  :pin "5d362ce98dcf656d7a55fcad6ae21c0a2caca861")
