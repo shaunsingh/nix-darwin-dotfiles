656d404cb1b5cc08412db22a843c0adcac1ce8ac
shaunsingh <shaunsingh0207@gmail.com>
2021-11-08 20:19:29 -0500

    updates

--- configs/doom/modules/completion/vertico/autoload/magit.el
+++
@@ -1,8 +0,0 @@
-;;; completion/vertico/autoload/magit.el -*- lexical-binding: t; -*-
-;;;###if (featurep! :tools magit)
-
-;;;###autoload
-(defun +vertico/embark-magit-status (file)
-  "Run `magit-status` on repo containing the embark target."
-  (interactive "GFile: ")
-  (magit-status (locate-dominating-file file ".git")))
