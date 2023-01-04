adde9ac3f482a6725cf49ee11e6f1522da4b84aa
shaunsingh <shaunsingh0207@gmail.com>
2023-01-03 19:51:20 -0500

    (mesa) try to enable graphics acceleration

--- hosts/mbp-m1-linux/default.nix
+++ hosts/mbp-m1-linux/default.nix
@@ -17,9 +17,18 @@
     ../../modules/linux.nix
   ];
 
+  # new kernel
   hardware.asahi.addEdgeKernelConfig = true;
+
+  # apple firmware
   hardware.asahi.peripheralFirmwareDirectory = ./firmware;
-  hardware.opengl.package = pkgs.asahi-mesa.drivers;
+
+  # enable graphics acceleration 
+  hardware.opengl.enable = true;
+  system.replaceRuntimeDependencies = [
+    ({ original = pkgs.mesa; replacement = pkgs.asahi-mesa; })
+    ({ original = pkgs.mesa.drivers; replacement = pkgs.asahi-mesa.drivers; })
+  ];
 
   system.stateVersion = "23.05";
 }
