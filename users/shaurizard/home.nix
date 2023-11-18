a5b4d34d6bf740fce6c383b16a7e6633fa4495a4
Shaurya Singh <shaunsingh0207@gmail.com>
2023-11-18 10:34:48 -0500

    yeah I can't think of a use for tmux either

--- users/shaurizard/home.nix
+++ users/shaurizard/home.nix
@@ -58,22 +58,6 @@
     };
   };
 
-  ### -- tmux
-  tmux = {
-    enable = true;
-    sensibleOnTop = true;
-    extraConfig = with config.colorscheme.colors; ''
-      set -g status-right-length 100
-      set -g status-left-length 100
-      set -g status-left " "
-      set -g status-right " "
-      set -g status-justify left
-      set -g status-style fg=black,bg=default
-      set -g window-status-current-format "#[fg=#${base00},bg=#${base0C}] #I #[fg=#${base05},bg=#${base01}] [#W] #[fg=#${base03},bg=#${base01}]#{s|$HOME|~|;s|.*/||:pane_current_path} "
-      set -g window-status-format "#[fg=#{base00},bg=#{base0F}] #I #[fg=#{base04},bg=#{base01}] [#W] #[fg=#{base03},bg=#{base01}]#{s|$HOME|~|;s|.*/||:pane_current_path} "
-    '';
-  };
-
   ### -- sway
   wayland.windowManager.sway = {
     enable = true;
